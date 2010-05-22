b3678754c0ca40b343a0920b65b6fe635838ce02
Curtis (Programble) <programble@gmail.com>
2010-05-22 14:10:58 -0400

    Rewrote reader

--- reader.py
+++ reader.py
@@ -1,87 +1,113 @@
 # Copyright 2010 Curtis (Programble) <programble@gmail.com>
 # Licensed under the GNU GPLv3
 
-# TODO: Make this reader not suck
-
 import string
-from StringIO import StringIO
 
 from lisp import *
 
-class ReaderError(Exception):
-    pass
-
 class Reader:
     def __init__(self, source):
-        self.stream = StringIO(source)
+        self.source = source + '\n'
+        self.index = -1
+
+    def current(self):
+        try:
+            return self.source[self.index]
+        except IndexError:
+            raise SyntaxError("unexpected EOF")
+
+    def next(self):
+        self.index += 1
+        return self.current()
+
+    def prev(self):
+        self.index -= 1
+        return self.current()
 
     def read(self):
         exprs = []
-        token = self.get_token()
-
-        while token != None:
-            if token == '(':
-                exprs.append(self.get_expr())
-            elif token == ')':
-                raise ReaderError("Unbalanced parens")
-            elif token == "'":
-                token = self.get_token()
-                if token == None:
-                    raise ReaderError("Unexpected EOF")
-                elif token == '(':
-                    exprs.append(List([Symbol("quote"), self.get_expr()]))
-                else:
-                    exprs.append(List([Symbol("quote"), token]))
-            elif all([x in string.digits for x in token]):
-                exprs.append(Atom(int(token)))
+        while self.index + 1 < len(self.source):
+            self.next()
+            if self.current() in string.whitespace:
+                # Skip over whitespace
+                continue
+            elif self.current() == '(':
+                exprs.append(self.read_list())
+            elif self.current() == ')':
+                raise SyntaxError("unexpected right paren")
+            elif self.current() == '"':
+                exprs.append(self.read_string())
+            elif self.current() in string.digits:
+                # Move back one
+                self.prev()
+                exprs.append(self.read_number())
+            elif self.current() == "'":
+                exprs.append(self.read_quote())
             else:
-                exprs.append(Symbol(token))
-            token = self.get_token()
+                # Move back one
+                self.prev()
+                exprs.append(self.read_symbol())
         return exprs
-        
-    def get_expr(self):
-        token = self.get_token()
 
-        expr = []
-        
-        while token != ')':
-            if token == None:
-                raise ReaderError("Unexpected EOF")
-            elif token == '(':
-                expr.append(self.get_expr())
-            elif token == "'":
-                token = self.get_token()
-                if token == None:
-                    raise ReaderError("Unexpected EOF")
-                elif token == '(':
-                    expr.append(List([Symbol("quote"), self.get_expr()]))
-                else:
-                    expr.append(List([Symbol("quote"), token]))
-            elif all([x in string.digits for x in token]):
-                expr.append(Atom(int(token)))
+    def read_list(self):
+        list = []
+        while self.next() != ')':
+            if self.current() in string.whitespace:
+                # Skip over whitespace
+                continue
+            elif self.current() == '(':
+                # Embedded list
+                list.append(self.read_list())
+            elif self.current() == '"':
+                list.append(self.read_string())
+            elif self.current() in string.digits:
+                # Move back one
+                self.prev()
+                list.append(self.read_number())
+            elif self.current() == "'":
+                list.append(self.read_quote())
             else:
-                expr.append(Symbol(token))
-            token = self.get_token()
+                # Move back one
+                self.prev()
+                list.append(self.read_symbol())
+        return List(list)
 
-        return List(expr)
+    def read_string(self):
+        string = ""
+        while self.next() != '"':
+            string += self.current()
+        return String(string)
 
-    def get_token(self):
-        token = self.stream.read(1)
-        if token == '':
-            # EOF
-            return None
-        elif token in string.whitespace:
-            return self.get_token()
-        elif token in "()'":
-            return token
+    def read_number(self):
+        number = ""
+        while self.next() not in string.whitespace + ")":
+            number += self.current()
+        if '.' in number:
+            try:
+                number = float(number)
+            except ValueError:
+                raise
         else:
-            while True:
-                x = self.stream.read(1)
-                if x == '':
-                    break
-                if x in string.whitespace or x == ")":
-                    self.stream.seek(-1, 1)
-                    break
-                token += x
-            return token
+            try:
+                number = int(number)
+            except ValueError:
+                raise
+        self.prev()
+        return Atom(number)
 
+    def read_symbol(self):
+        symbol = ""
+        while self.next() not in string.whitespace + ")":
+            symbol += self.current()
+        self.prev()
+        return Symbol(symbol)
+
+    def read_quote(self):
+        expr = [Symbol("quote")]
+        self.next()
+        if self.current() == '(':
+            expr.append(self.read_list())
+        else:
+            self.prev()
+            expr.append(self.read_symbol())
+        return List(expr)
