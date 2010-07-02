7ca60996c930c3274526d2ede0225eeb1f5bb7c3
Curtis (Programble) <programble@gmail.com>
2010-07-02 00:11:03 -0400

    Added some core lisp macros

--- core.lisp
+++ core.lisp
@@ -1,3 +1,29 @@
 ;; Copyright 2010 Curtis McEnroe <programble@gmail.com>
 ;; Licensed under the GNU GPLv3
 
+;; Def-related macros
+(def defmacro (macro (n a & b) `(def ,n (macro ,a ,@b))))
+(defmacro defun (n a & b) `(def ,n (lambda ,a ,@b)))
+
+;; Logical operators
+(defmacro and (x y) `(cond (,x (cond (,y t) (t nil))) (t nil)))
+(defmacro not (x) `(cond (,x nil) (t t)))
+(defmacro nand (x y) `(not (and ,x ,y)))
+(defmacro or (x y) `(nand (nand ,x ,x) (nand ,y ,y)))
+(defmacro xor (x y) `(or (and ,x (not ,y)) (and (not ,x) ,y)))
+
+;; Comparison operators
+(defmacro <= (x y) `(or (= ,x ,y) (< ,x ,y)))
+(defmacro >= (x y) `(or (= ,x ,y) (> ,x ,y)))
+(defmacro != (x y) `(not (= ,x ,y)))
+
+;; Common List Functions
+(defmacro caar (x) `(car (car ,x)))
+(defmacro cddr (x) `(cdr (cdr ,x)))
+(defmacro cadr (x) `(car (cdr ,x)))
+(defmacro caddr (x) `(car (cdr (cdr ,x))))
+(defmacro cadar (x) `(car (cdr (car ,x))))
+(defmacro caddar (x) `(car (cdr (cdr (car ,x)))))
+
+;; Predicates
+(defmacro nil? (x) `(cond (,x nil) (t)))
